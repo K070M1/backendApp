ebf795d3d0637ae4411b0318055a49f4d133282c
K070M1 <animeme_15@hotmail.com>
2024-02-18 14:09:38 -0500

    Document Type

--- src/main/java/com/backend/springboot/model/DocumentType.java
+++ src/main/java/com/backend/springboot/model/DocumentType.java
@@ -6,12 +6,14 @@ import java.util.UUID;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document("stateType")
-public class StateType {
-	
+import jakarta.validation.constraints.NotNull;
+
+@Document("documentType")
+public class DocumentType {
 	@Id
 	private String _id;
 	
@@ -27,90 +29,83 @@ public class StateType {
 	@Field("updated_at")
 	private LocalDateTime updatedAt;
 	
+	@NotNull
+	@DBRef
+	private StateType stateType_id;
 	
-	
-	public StateType() {
+	// RANDOM FOR CODE
+	public DocumentType() {
 		if(this.cod == null) {
 			this.cod = UUID.randomUUID().toString().replace("-", "").toUpperCase();			
 		}
 	}
-	
-	public StateType(String _id, String name, String cod, LocalDateTime createdAt, LocalDateTime updatedAt) {
+
+	public DocumentType(String _id, String name, String cod, LocalDateTime createdAt, LocalDateTime updatedAt,
+			@NotNull StateType stateType_id) {
 		super();
 		this._id = _id;
 		this.name = name;
 		this.cod = cod;
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
+		this.stateType_id = stateType_id;
 	}
-
-
 
 	public String get_id() {
 		return _id;
 	}
 
-
-
 	public void set_id(String _id) {
 		this._id = _id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getCod() {
 		return cod;
 	}
 
-
-
 	public void setCod(String cod) {
 		this.cod = cod;
 	}
-
-
 
 	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
 
-
-
 	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
-
-
 
 	public LocalDateTime getUpdatedAt() {
 		return updatedAt;
 	}
 
-
-
 	public void setUpdatedAt(LocalDateTime updatedAt) {
 		this.updatedAt = updatedAt;
 	}
 
+	public StateType getStateType_id() {
+		return stateType_id;
+	}
 
+	public void setStateType_id(StateType stateType_id) {
+		this.stateType_id = stateType_id;
+	}
 
 	@Override
 	public String toString() {
-		return "StateType [_id=" + _id + ", name=" + name + ", cod=" + cod + ", createdAt=" + createdAt + ", updatedAt="
-				+ updatedAt + "]";
+		return "DocumentType [_id=" + _id + ", name=" + name + ", cod=" + cod + ", createdAt=" + createdAt
+				+ ", updatedAt=" + updatedAt + ", stateType_id=" + stateType_id + "]";
 	}
+
+	
 	
 	
 }
